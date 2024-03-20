bf91719d7ccfa213cd40fd129f388cbce55a51ab
diana <gududiana@gmail.com>
2024-03-20 23:07:04 +0300

    completed labs

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+
+// Declare customerName to be bob in global scope
+var customerName = "bob";
+
+// Define upperCaseCustomerName function
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+// Define setBestCustomer function
+function setBestCustomer() {
+    bestCustomer = "not bob";
+}
+
+// Define overwriteBestCustomer function
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";
+}
+
+// Define changeLeastFavoriteCustomer function
+function changeLeastFavoriteCustomer() {
+    const leastFavoriteCustomer = "not bob";
+    leastFavoriteCustomer = "still not bob"; // This will throw an error because you cannot reassign a constant
+}
